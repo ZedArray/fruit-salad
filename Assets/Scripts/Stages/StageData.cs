e9153ed87f663591682936e5fb3cd60b20a980c9
graphicmismatch <rayan.madan.2005@gmail.com>
2026-06-17 13:59:43 +0530

    stagedata

--- Assets/Scripts/Stages/StageData.cs
+++ Assets/Scripts/Stages/StageData.cs
@@ -1,7 +1,25 @@
+using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
+
+public abstract class StageComponent : MonoBehaviour
+{
+    public bool doStage;
+    public void enable()
+    {
+        doStage = true;
+    }
+
+    public void disable()
+    {
+        doStage = false;
+    }
+}
 
 [CreateAssetMenu(fileName = "StageData", menuName = "Scriptable Objects/StageData")]
 public class StageData : ScriptableObject
 {
+    public StageComponent[] stageManagers;
     
 }
+
